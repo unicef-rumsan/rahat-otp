06dc81070d1883925139d39fab09bf97a871bb84
Santosh <santosh@rumsan.com>
2022-12-09 13:14:04 +0545

    debug_mode

--- src/plugins/sms/index.js
+++ src/plugins/sms/index.js
@@ -2,6 +2,18 @@ const config = require('config');
 const { MailService } = require('@rumsan/core/services');
 
 module.exports = async (phone, message) => {
+  if (config.has('debug_mode') && config.get('debug_mode')) {
+    phone = `${phone}@mailinator.com`;
+    MailService.send({
+      to: phone,
+      subject: 'Rumsan-SMS: Test Email',
+      html: message
+    }).then(e => {
+      console.log(`Test email to: ${phone}`);
+    });
+    return;
+  }
+
   if (!config.get('enabled')) {
     console.log('ERROR: SMS service is disabled.');
     MailService.send({
