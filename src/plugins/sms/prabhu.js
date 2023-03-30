7edd6473146da4a1a81f58a186fdf1c4f7d5c21f
myanzik <myanz.470shrestha@gmail.com>
2023-03-30 16:06:44 +0545

    updated prabhu sms api

--- src/plugins/sms/prabhu.js
+++ src/plugins/sms/prabhu.js
@@ -1,5 +1,6 @@
 const config = require('config');
 const axios = require('axios');
+const querystring = require('node:querystring');
 
 const url = config.get('services.prabhu.url');
 const token = config.get('services.prabhu.token');
@@ -7,10 +8,13 @@ const token = config.get('services.prabhu.token');
 module.exports = async (phone, message) => {
   if (!phone) throw new Error('No receipent was specified');
   if (!message) throw new Error('No Message was specified');
-  return axios.post(`${url}?token=${token}`, [
-    {
-      Message: message,
-      MobileNumber: phone
-    }
-  ]);
+  // return axios.post(`${url}?token=${token}`, [
+  //   {
+  //     Message: message,
+  //     MobileNumber: phone
+  //   }
+  // ]);
+
+  const params = querystring.stringify({ to: phone, content: message, token });
+  return axios.get(`${url}?${params}`);
 };
